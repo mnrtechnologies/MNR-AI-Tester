a3bf9500fa0382be16964770efd2ec64f808202b
pratikmehakare <pratikmehakare8@gmail.com>
2026-04-13 13:14:29 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ const subsRoute = require("./routes/subscriptionRoutes")
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 const app = express();
-app.set("trust proxy", 1);
 
 /**
  * ----------------------------------------------------
